8961d52ebf66fe1232f9e570fa5c038c07dc8bff
Marc Altmann <marcaltmann@posteo.de>
2020-09-12 16:20:12 +0200

    Add some tests

--- test/controllers/hello_controller_test.rb
+++ test/controllers/hello_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class HelloControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get api route" do
+    get api_people_url, xhr: true
+
+    assert_response :success
+    assert_equal "application/json", @response.media_type
+
+    json = JSON.parse @response.body
+    assert_match "Nolan", @response.body
+    assert_match "Hitchcock", @response.body
+    assert_equal "Array", json.class.name
+    assert_equal 2, json.length
+    assert json.is_a?(Array)
+  end
 end
